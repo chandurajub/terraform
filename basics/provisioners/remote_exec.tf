77d3910447cf7e022ac752bfad4cf3560292f3f4
chandurajub <chandu.raju.abc@gmail.com>
2020-07-02 22:03:52 +0530

    git

---
+++ basics/provisioners/remote_exec.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "xyz" {
+  ami = "ami-0c84a3e93390c29bc"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-003d8b06a6a750275"]
+
+provisioner "remote-exec" {
+  connection {
+    type = "ssh"
+    user = "root"
+    password = "DevOps321"
+    host = self.public_ip
+  }
+  inline = ["cd /root",
+             "git clone https://chandurajub:Chandu@1234567890github.com/chandurajub/terraform.git"
+           ]
+}
+
+  }
+
