01ee18dafbb1eff709d52682359815c7460bc469
chandurajub <chandu.raju.abc@gmail.com>
2020-07-02 21:08:18 +0530

    git

--- basics/ec2/ec2.tf
+++ basics/ec2/ec2.tf
@@ -4,6 +4,6 @@ provider "aws" {
 
 resource "aws_instance" "xyz" {
   ami = "ami-0c84a3e93390c29bc"
-  instance_type = "t2.micro"
-
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-003d8b06a6a750275"]
 }
