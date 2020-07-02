41275077fcd22a32a93326189907a99f7707aeff
chandurajub <chandu.raju.abc@gmail.com>
2020-07-02 14:06:55 +0530

    print message

--- basics/print.tf
+++ basics/print.tf
@@ -5,3 +5,47 @@ variable "name" {
 output "hello" {
   value = var.name
 }
+
+variable "st" {
+  default = "string"
+}
+ variable "n" {
+   default = 20
+ }
+variable "c" {
+  default = true
+}
+
+variable "list" {
+  default = [a,b,c]
+}
+
+variable "map" {
+default = {"a"="1",
+  "b"="2"}
+}
+
+variable "input" {}
+
+output "string" {
+  value = var.st
+}
+output "number" {
+  value = var.n
+}
+output "boolean" {
+  value = var.c
+}
+
+output "list" {
+  value = var.list
+}
+output "map" {
+  value = var.map
+}
+
+
+output "input" {
+  value = var.input
+}
+
