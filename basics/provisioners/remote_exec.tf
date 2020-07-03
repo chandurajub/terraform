774e3195ca3f6539fe6c7ebd66f3418c6f8b9814
chandurajub <chandu.raju.abc@gmail.com>
2020-07-03 14:37:50 +0530

    git

--- basics/provisioners/remote_exec.tf
+++ basics/provisioners/remote_exec.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "xyz" {
   ami = "ami-0c84a3e93390c29bc"
-  instance_type = "t3.medium"
+  instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-003d8b06a6a750275"]
 
 provisioner "remote-exec" {
@@ -11,11 +11,7 @@ provisioner "remote-exec" {
     host = self.public_ip
   }
   inline = [
-    "export GIT_USER=chandurajub",
-    "export GIT_PASSWORD=Chandu1234",
-    "git clone https://chandurajub:Chandu1234@gitlab.com/batch46/shell-scripting.git",
-
-    "sh shell-scripting/robo-shop-project/project.sh"
+    "abc"
 
            ]
 }
