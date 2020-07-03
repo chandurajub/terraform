88958a866240a3a2fe932eb4ec3a833d08ef9c26
chandurajub <chandu.raju.abc@gmail.com>
2020-07-03 13:32:26 +0530

    git

--- basics/provisioners/remote_exec.tf
+++ basics/provisioners/remote_exec.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "xyz" {
   ami = "ami-0c84a3e93390c29bc"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-003d8b06a6a750275"]
 
 provisioner "remote-exec" {
@@ -10,7 +10,7 @@ provisioner "remote-exec" {
     password = "DevOps321"
     host = self.public_ip
   }
-  inline = ["git clone https://github.com/chandurajub/terraform.git"
+  inline = ["yu install"
            ]
 }
 
