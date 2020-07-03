ba43f9a8932f727a9f55b0c54f5fcc8b34c868ec
chandurajub <chandu.raju.abc@gmail.com>
2020-07-04 00:13:04 +0530

    git

--- basics/modules/ec2andsg/main.tf
+++ basics/modules/ec2andsg/main.tf
@@ -6,6 +6,6 @@ module "sg" {
 
 module "ec2"  {
   source = "./ec2"
-  sg_id = var.sg_id
+  sg_id = module.sg.aws_security_group.allow_ssh.id
 }
 
--- basics/modules/ec2andsg/vars.tf
+++
@@ -1,3 +0,0 @@
-variable "sg_id" {
-default = module.sg.sg_id
-}
