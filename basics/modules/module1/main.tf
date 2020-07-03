5eefbab0fc406522b239c6bfcc98d612bbe5a32a
chandurajub <chandu.raju.abc@gmail.com>
2020-07-03 16:13:57 +0530

    git

--- basics/modules/module1/main.tf
+++ basics/modules/module1/main.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "sample" {
   provisioner "local-exec" {
-    command = "echo from module1"
+
     command = "$var.input"
   }
 }
--- basics/modules/module2/main.tf
+++ basics/modules/module2/main.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "sample" {
   provisioner "local-exec" {
-    command = "echo from module2"
+
     command = "$var.input"
   }
 }
