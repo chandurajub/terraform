745c7ae6be4984021b3ec53cc82c4f9dd7cb3665
chandurajub <chandu.raju.abc@gmail.com>
2020-07-06 00:00:54 +0530

    git

--- basics/locals/locals.tf
+++ basics/locals/locals.tf
@@ -1,7 +1,7 @@
 //to assign a name to an expression.
 
 locals {
- name = var.service_name["name"]-var.service_name["env"]-var.service_name["region"]-var.service_name["domain"]
+ name = "${var.service_name["name"]}-${var.service_name["env"]}-${var.service_name["region"]}-${var.service_name["domain"]}"
 
 }
 
