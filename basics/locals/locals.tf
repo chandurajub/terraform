8776eba7cc59b6e05505e89958e8d4f95d4cf997
chandurajub <chandu.raju.abc@gmail.com>
2020-07-05 23:53:28 +0530

    git

--- basics/locals/locals.tf
+++ basics/locals/locals.tf
@@ -4,11 +4,15 @@ locals {
  name = var.service_name
 }
 
-variable "service_name" {
-  name = "cart",
-  region = "us-west-2",
-  domain = "devops.in",
-  env = "dev"
+variable "service_name"
+{
+  default=
+  {
+    name = "cart",
+    region = "us-west-2",
+    domain = "devops.in",
+    env = "dev"
+  }
 }
 output "service_name" {
   value = var.service_name["name"]-var.service_name["env"]-var.service_name["region"]-var.service_name["domain"]
