17ae45ba8cfa8cece14a22cffe0845fd67282fcd
chandurajub <chandu.raju.abc@gmail.com>
2020-07-06 00:27:16 +0530

    git

--- basics/locals/locals.tf
+++ basics/locals/locals.tf
@@ -5,18 +5,12 @@ provider "aws" {
 }
 
 variable "service_name" {
-  default =
-  {
-    name = "cart",
-    region = "us-west-2",
-    domain = "devops.in",
-    env = "dev"
-  }
+  default = "hello"
+
 }
 
 
 
 output "servicee" {
-  value = "${var.service_name["name"]}-${var.service_name["env"]}-${var.service_name["region"]}-${var.service_name["domain"]}"
-
+  value = var.service_name
 }
