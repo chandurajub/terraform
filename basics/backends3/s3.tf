b0f151031f41289242052ef9fdb008a8014c29b5
chandurajub <chandu.raju.abc@gmail.com>
2020-07-06 17:46:04 +0530

    git

--- basics/backends3/s3.tf
+++ basics/backends3/s3.tf
@@ -1,15 +1,22 @@
 
 
-terraform {
-  backend "s3" {
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
     bucket = "masterbucket01"
     key    = "sample/state.tf"
     region = "us-west-2"
   }
 }
 
+
 resource "null_resource" "null" {
   provisioner "local-exec" {
     command = "echo welcome"
+  }
+}
+resource "null_resource" "null01" {
+  provisioner "local-exec" {
+    command = "sleep 90"
   }
 }
