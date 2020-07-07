aef477295c1ed019b363d1dacc42ec6206a49707
chandurajub <chandu.raju.abc@gmail.com>
2020-07-07 16:32:40 +0530

    git

--- VPC/makefile.mk
+++ VPC/makefile.mk
@@ -7,12 +7,10 @@ help:
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[35m%-10s\033[0m %s\n", $$1, $$2}'
 
 dev: ## Apply Module in Prod Environement
-
 	@terraform init
 	@terraform apply -auto-approve -var-file=devcidr.tfvars
 
 prod: ## Apply Module in NonProd Environement
-
 	@terraform init
 	@terraform apply -auto-approve -var-file=prodcidr.tfvars
 
